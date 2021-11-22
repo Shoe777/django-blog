fa3216c2bf163e2802b83c6366e7d79bca29072e
Shoe777 <69448596+Shoe777@users.noreply.github.com>
2021-11-21 16:39:59 -0800

    convert function-base view into class-based

--- blogging/views.py
+++ blogging/views.py
@@ -3,6 +3,8 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
@@ -18,20 +20,31 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type = "text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    #template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    #body = template.render(context)
-    #return HttpResponse(body, content_type='text/html')
-    return render(request, 'list.html', context)
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     #template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     #body = template.render(context)
+#     #return HttpResponse(body, content_type='text/html')
+#     return render(request, 'list.html', context)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'detail.html', context)     
+class BlogListView(ListView):
+    #queryset = Post.object.all()
+    queryset = Post.objects.filter().exclude(published_date__exact=None).order_by('-published_date')
+    model = Post
+    template_name = 'list.html'
+
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'detail.html', context)
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'detail.html'
